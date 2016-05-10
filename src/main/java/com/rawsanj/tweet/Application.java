2b44680ee19d877d6c88e63622f25e5a4190f217
Sanjay Rawat <sanjayprawat31@gmail.com>
2016-05-11 01:54:47 +0530

    Jar to Executable War

--- src/main/java/com/rawsanj/tweet/Application.java
+++ src/main/java/com/rawsanj/tweet/Application.java
@@ -4,15 +4,22 @@ package com.rawsanj.tweet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 import org.springframework.social.twitter.api.impl.TwitterTemplate;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer{
 	
 	@Autowired
     private Environment environment;
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
